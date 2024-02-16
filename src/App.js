180b736cb8cca7f616de59fb6a560a1ac1db0a64
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2024-02-16 14:03:42 +0530

    Set up React app and project structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Profile</h2>
+      </div>
+      <div>
+      <div>
+      <div>
+        <h3>Rich Explorer</h3>
+      </div>
+      </div>
+
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
